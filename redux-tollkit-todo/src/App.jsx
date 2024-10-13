58a7933af2b34eb47ebdfeeddbd03635eaac6548
Aman8593 <amanwadgaonkar8593@gmail.com>
2024-10-13 23:25:33 +0530

    redux-toolit with todo

---
+++ redux-tollkit-todo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todo from './Components/Todo'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
